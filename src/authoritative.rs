20c2af01fb9705d3c7e032b438dcd90f32b3f3be
Marcus Ball <marcus@marcusball.me>
2016-03-15 00:33:47 -0400

    I guess you've got to start somewhere

---
+++ src/authoritative.rs
@@ -0,0 +1,126 @@
+extern crate mio;
+
+use client::GameClient;
+
+use mio::{TryRead, TryWrite};
+use mio::tcp::*;
+use mio::util::Slab;
+use mio::Token;
+use mio::{EventLoop, EventSet, PollOpt, Handler};
+use bytes::{Buf, Take};
+use std::mem;
+use std::net::SocketAddr;
+use std::io::Cursor;
+use std::thread;
+use std::sync::mpsc;
+use std::sync::{Arc, RwLock};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::collections::HashMap;
+
+
+const SERVER_TOKEN: mio::Token = mio::Token(1);
+
+#[derive(Clone)]
+pub struct AuthoritativeServerState{
+    clients: Arc<RwLock<Slab<GameClient>>>,
+    token_counter: Arc<AtomicUsize>
+}
+
+impl AuthoritativeServerState{
+    pub fn new() -> AuthoritativeServerState{
+        AuthoritativeServerState{
+            token_counter: Arc::new(AtomicUsize::new(1)),
+            // Max 128 connections
+            clients: Arc::new(RwLock::new(Slab::new_starting_at(Token(2), 128)))
+        }
+    }
+}
+
+pub struct AuthoritativeServer{
+    // The socket on which this server is listening
+    socket: TcpListener,
+
+    // The token assigned to this server's own connection
+    token: Token,
+
+    // Current state for the server
+    state: AuthoritativeServerState
+}
+
+impl AuthoritativeServer{
+    pub fn new(address: SocketAddr) -> AuthoritativeServer{
+        let server_state = AuthoritativeServerState::new();
+        let server_state_clone = server_state.clone();
+
+        println!("Starting authoritative server");
+        let server_socket = TcpListener::bind(&address).expect("Failed to start socket listener!");
+
+        let mut server = AuthoritativeServer{
+            socket: server_socket,
+            token: SERVER_TOKEN,
+            state: server_state_clone
+        };
+
+        let mut event_loop = EventLoop::new().expect("Failed to create server event loop!");
+
+        event_loop.register(&server.socket,
+                            SERVER_TOKEN,
+                            EventSet::readable(),
+                            PollOpt::edge()).expect("Failed to register server with event loop!");
+
+        println!("Running event loop...");
+        event_loop.run(&mut server).expect("Failed to run event loop!");
+
+        return server;
+    }
+}
+
+impl Handler for AuthoritativeServer{
+    type Timeout = ();
+    type Message = ();
+
+    fn tick(&mut self, event_loop: &mut EventLoop<AuthoritativeServer>) {
+        println!("Begin server tick!");
+
+
+        println!("End server tick!");
+    }
+
+    fn ready(&mut self, event_loop: &mut EventLoop<AuthoritativeServer>, token: Token, events: EventSet) {
+        assert!(token != Token(0), "We're not supposed to get a Token(0)!");
+
+        if events.is_error(){
+            println!("Error event for token {:?}", token);
+            //Reset token?
+            return;
+        }
+
+        if events.is_hup(){
+            println!("OH FUCK NO, {:?} DID NOT JUST FUCKING HANG UP ON ME!", token);
+            println!("I'M GOING TO FUCKING MURDER YOU FUCKER");
+            //Reset?
+            return;
+        }
+
+        if events.is_writable(){
+            println!("Oh shit, motherfucking {:?} is writable! Look at this guy!", token);
+
+            //fucking write some shit
+        }
+        else{
+            println!("[{:?} silence intensifies]", token);
+        }
+
+        if events.is_readable(){
+            println!("GOT SHIT TO READ FROM MY BRAH {:?} HELLLL YEAH", token);
+
+            if self.token == token{
+                //I should accept this event loop or something?
+            }
+            else{
+                //Read some shit
+            }
+        }
+    }
+
+}
---
+++ src/client.rs
@@ -0,0 +1,4 @@
+
+pub struct GameClient{
+    null: u8
+}
